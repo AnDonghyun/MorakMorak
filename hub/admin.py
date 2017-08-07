c4527cd0736e53b00d7ec162c2ad98456e42355e
Donghyun An <miehdan@gmail.com>
2017-08-07 23:46:14 +0900

    admin에 model 등록

--- hub/admin.py
+++ hub/admin.py
@@ -1,4 +1,7 @@
 # hub/admin.py
 from django.contrib import admin
+from .models import Mylist
 
 # Register your models here.
+
+admin.site.register(Mylist)
--- hub/models.py
+++ hub/models.py
@@ -2,3 +2,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Mylist(models.Model):
+    pass
