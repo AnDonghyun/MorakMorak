aa1aac34c6fc8ec1ebd011b2a01ff3be5d0d6a43
Donghyun An <miehdan@gmail.com>
2017-08-13 23:38:24 +0900

    hub/views/list 추가

--- hub/urls.py
+++ hub/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [ 
-    # url(r'^$', views.list, name='list'),
+    url(r'^$', views.list, name='hub_list'),
     url(r'^mynovel/$', views.mynovel, name='my_novel'),
     url(r'^sharednovel/$', views.sharednovel, name='shared_novel'),
 
--- hub/views.py
+++ hub/views.py
@@ -5,11 +5,17 @@ from .models import MyNovel
 # Create your views here.
 def mynovel(request):
     my_novel = MyNovel.objects.all()
-    return render(request, 'hub/mynovel.html')
+    return render(request, 'hub/mynovel.html', {
+        'novel_list': my_novel,
+        })
 
 
 def sharednovel(request):
     shared_novel = MyNovel.objects.all()
     return render(request, 'hub/sharednovel.html',{
-        'sharednovel': shared_novel
+        'novel_list': shared_novel,
         })
+
+
+def list(request):
+    return render(request, 'hub/hublist.html')
