6f167c36d8bee9d69a75b4899e8f94e3d98f1787
ssexyryan <joonkim3@korea.ac.kr>
2017-09-09 11:10:44 +0900

    DB비밀번호 정상화

--- morakmorak/settings.py
+++ morakmorak/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'morak',
         'USER': 'root',
-        'PASSWORD': 'morakmorak!',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '3306',
     }
